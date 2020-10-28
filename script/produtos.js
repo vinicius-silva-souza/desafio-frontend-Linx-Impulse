82a99c7e58de90be946296aa23afc28a6d53ad0b
Vinicius da Silva Souza <vinicius.silva.souza@outlook.com>
2020-10-28 19:05:11 -0300

    Final do desenvolvimento do LAYOUT da seção PRODUTOS

--- script/produtos.js
+++ script/produtos.js
@@ -1,5 +1,9 @@
-var xhr = new XMLHttpRequest();
-var url = 'https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page='
-xhr.open('GET', url, true);
-xhr.send();
-xhr.response;
+function carregarProdutos(produtos){
+    var xhr = new XMLHttpRequest();
+    var url = 'https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page='
+    xhr.open('GET', url, true);
+    xhr.send();
+    xhr.addEventListener('load', ()=>{
+        produtos(xhr.response)
+    });
+}
