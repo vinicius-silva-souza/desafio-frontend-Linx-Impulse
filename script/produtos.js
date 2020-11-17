97ee7f10f2b595618a7f72e8524c2679e792174f
Vinicius da Silva Souza <vinicius.silva.souza@outlook.com>
2020-11-16 22:42:58 -0300

    Correção de LAYOUT da seção PRODUTOS e início de consumo de API

--- script/produtos.js
+++ script/produtos.js
@@ -1,9 +1,21 @@
+var numPagina = 1;
+var btnMaisProdutos = document.getElementById('btn-mais-produtos');
+
 function carregarProdutos(produtos){
     var xhr = new XMLHttpRequest();
-    var url = 'https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page='
+    var url = `https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=${numPagina}`;
+    xhr.responseType = "json";
     xhr.open('GET', url, true);
     xhr.send();
     xhr.addEventListener('load', ()=>{
         produtos(xhr.response)
+        numPagina++;
     });
 }
+
+btnMaisProdutos.addEventListener('click', ()=>{
+    carregarProdutos((produtos)=>{
+        console.log(produtos);
+    })
+})
+
