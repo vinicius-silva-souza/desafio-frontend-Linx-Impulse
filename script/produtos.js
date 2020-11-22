fa66c29eedcd34483b32c840d275be9855f0fa2a
Vinicius da Silva Souza <vinicius.silva.souza@outlook.com>
2020-11-22 17:57:08 -0300

    Requisição AJAX de produtos via API finalizada

--- script/produtos.js
+++ script/produtos.js
@@ -1,21 +1,72 @@
-var numPagina = 1;
-var btnMaisProdutos = document.getElementById('btn-mais-produtos');
-
-function carregarProdutos(produtos){
+function productsRequest(data){
     var xhr = new XMLHttpRequest();
-    var url = `https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=${numPagina}`;
+    var url = 'https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1';
     xhr.responseType = "json";
     xhr.open('GET', url, true);
     xhr.send();
     xhr.addEventListener('load', ()=>{
-        produtos(xhr.response)
-        numPagina++;
+        data(xhr.response)
     });
 }
 
-btnMaisProdutos.addEventListener('click', ()=>{
-    carregarProdutos((produtos)=>{
-        console.log(produtos);
-    })
-})
+function createCards(description, image, count, value, name, oldPrice, price){
+    var element = document.getElementById("produtos-container");
+    var createdElement = document.createElement('div');
+    element = element.appendChild(createdElement);
+    element.setAttribute('class', 'produto');
+    createdElement = document.createElement('img');
+    element =  element.appendChild(createdElement);
+    element.setAttribute('class', 'img-produto');
+    element.setAttribute('src', image);
+    createdElement = document.createElement('div');
+    element = element.insertAdjacentElement('afterend', createdElement);
+    element.setAttribute('class', 'info-produto');
+    createdElement = document.createElement('p');
+    element = element.appendChild(createdElement);
+    element.setAttribute('class', 'nome-produto');
+    element.innerText = name;
+    createdElement = document.createElement('p');
+    element = element.insertAdjacentElement('afterend', createdElement);
+    element.setAttribute('class', 'descricao-produto');
+    element.innerText = description;
+    createdElement = document.createElement('small');
+    element = element.insertAdjacentElement('afterend', createdElement);
+    element.setAttribute('class', 'preco-produto');
+    element.innerText = `De: R$${oldPrice}`;
+    createdElement = document.createElement('strong');
+    element = element.insertAdjacentElement('afterend', createdElement);
+    element.setAttribute('class', 'oferta-produto');
+    element.innerText = `Por: R$${price}`;
+    createdElement = document.createElement('small');
+    element = element.insertAdjacentElement('afterend', createdElement);
+    element.setAttribute('class', 'parcela-produto');
+    element.innerText = `ou ${count}x de R$${value}`
+    createdElement = document.createElement('button');
+    element = element.insertAdjacentElement('afterend', createdElement);
+    element.setAttribute('class', 'btn-comprar-produto btn');
+    element.setAttribute('type', 'button');
+    element.innerText = 'Comprar'
+}
 
+function request(){
+    productsRequest((produtos)=>{
+        var json = produtos;
+        var produtos = json.products;
+        produtos.forEach((element)=> {
+           createCards(
+               element.description,
+               element.image,
+               element.installments.count,
+               element.installments.value,
+               element.name,
+               element.oldPrice,
+               element.price
+           )
+        });
+    })
+}
+
+window.addEventListener('load', request);
+
+const btnMaisProdutos = document.getElementById('btn-mais-produtos');
+btnMaisProdutos.addEventListener('click', request);
